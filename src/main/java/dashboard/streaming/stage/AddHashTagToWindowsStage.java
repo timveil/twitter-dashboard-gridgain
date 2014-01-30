c6117b354e730569b9df3eb30f91f307968cfe40
tveil <tveil@gridgain.com>
2014-01-30 13:36:13 -0500

    upgrading dependencies

--- src/main/java/dashboard/streaming/stage/AddHashTagToWindowsStage.java
+++ src/main/java/dashboard/streaming/stage/AddHashTagToWindowsStage.java
@@ -56,9 +56,9 @@ public class AddHashTagToWindowsStage implements GridStreamerStage<TweetVO> {
             }
 
 
-            evict(fiveMinuteWindow);
-            evict(fifteenMinuteWindow);
-            evict(sixtyMinuteWindow);
+            //evict(fiveMinuteWindow);
+            //evict(fifteenMinuteWindow);
+            //evict(sixtyMinuteWindow);
 
         }
 
