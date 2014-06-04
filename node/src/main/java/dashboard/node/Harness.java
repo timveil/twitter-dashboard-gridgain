3b622221d1d0c43a2f20d82ad9ea48a564fd048d
tveil <tveil@gridgain.com>
2014-06-04 16:27:49 -0400

    using constants in xml; moving default jetty port

--- node/src/main/java/dashboard/node/Harness.java
+++ node/src/main/java/dashboard/node/Harness.java
@@ -7,11 +7,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Harness {
 
+    /*
+
+        Start node with the following system property to avoid 8080 web conflicts
+
+        -DGRIDGAIN_JETTY_PORT=8090
+
+
+     */
 
     public static void main(String[] args) throws GridException {
 
         final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("grid-gain.xml");
         final GridConfiguration config = ctx.getBean(GridConfiguration.class);
+
 
         GridGain.start(config);
 
